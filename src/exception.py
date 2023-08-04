d229af372a960ae7f52aaa177d6c22dc1def3364
kumari <Jyoti.kumari.2@team.telstra.com>
2023-08-04 22:14:04 +0530

    exceptions handling

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+import logger
+import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -10,5 +12,18 @@ def error_message_detail(error, error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message, error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
+if __name__=="__main__": 
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by Zero")
+        raise CustomException(e,sys)
+    
+    
